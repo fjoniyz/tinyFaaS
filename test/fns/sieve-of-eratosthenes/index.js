d8371b77a28f760e5989f2a238d56544122784b1
F Yzeiri <fjoniyzeiri@gmail.com>
2023-12-04 11:46:29 +0100

    added sleep for sieve

--- test/fns/sieve-of-eratosthenes/index.js
+++ test/fns/sieve-of-eratosthenes/index.js
@@ -1,20 +1,27 @@
-
 module.exports = (req, res) => {
-  const max = 10000;
-  let sieve = [], i, j, primes = [];
-  for (i = 2; i <= max; ++i) {
-
-    if (!sieve[i]) {
-      primes.push(i);
-      for (j = i << 1; j <= max; j += i) {
-        sieve[j] = true;
+  // Function to generate primes
+  const generatePrimes = () => {
+    const max = 10000;
+    let sieve = [],
+      i,
+      j,
+      primes = [];
+    for (i = 2; i <= max; ++i) {
+      if (!sieve[i]) {
+        primes.push(i);
+        for (j = i << 1; j <= max; j += i) {
+          sieve[j] = true;
+        }
       }
     }
-  }
 
-  response = ("Found " + primes.length + " primes under " + max);
+    response = "Found " + primes.length + " primes under " + max;
 
-  console.log(response);
+    console.log(response);
 
-  res.send(response + "\n");
-}
+    res.send(response + "\n");
+  };
+
+  // Add a sleep of 5 seconds
+  setTimeout(generatePrimes, 5000);
+};
